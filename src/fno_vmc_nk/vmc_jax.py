e5575b0af311eb8eacfefe82318b0817846cb235
zhengshizhao <shizhao.zheng@mail.utoronto.ca>
2025-07-01 23:50:55 -0700

    Baseline Experiments

--- src/fno_vmc_nk/vmc_jax.py
+++ src/fno_vmc_nk/vmc_jax.py
@@ -11,7 +11,7 @@ from flax.core.frozen_dict import freeze, unfreeze
 
 ENERGY_MIN, ENERGY_MAX = -100000, 100000
 
-SLATER_STEPS = 500
+SLATER_STEPS = 700
 
 def label_fn(path, _):
     return "slater" if path[0] == "slater" else "backflow"
