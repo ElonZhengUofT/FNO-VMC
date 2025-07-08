4dca4707db19a4df30cd1576f78f76dce82ef250
zhengshizhao <shizhao.zheng@mail.utoronto.ca>
2025-07-07 17:47:20 -0700

    Baseline Experiments on 1D

--- src/fno_vmc_nk/ansatz/BackflowFNO.py
+++ src/fno_vmc_nk/ansatz/BackflowFNO.py
@@ -108,7 +108,7 @@ class NNBackflowSlater2nd(nn.Module):
         if n.ndim == 1:
             return single_logdet(n, F_flat)
         else:
-            batch_size = 512  # 你可以根据显存/性能自行调节
+            batch_size = 1024  # 你可以根据显存/性能自行调节
             logdets = []
             # 按 batch_size 切块
             for i in range(0, n.shape[0], batch_size):
