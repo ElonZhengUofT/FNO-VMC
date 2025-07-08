1e454c7bdb35824b5285a0ac552f72e9f72f2c17
zhengshizhao <shizhao.zheng@mail.utoronto.ca>
2025-07-07 18:23:13 -0700

    Baseline Experiments on 1D

--- src/scripts/run_train.sh
+++ src/scripts/run_train.sh
@@ -6,7 +6,8 @@ ssh -X shizhao@131.215.142.161 << 'EOF'
   cd Projects
   cd FNO-VMC
   git pull
-  #export XLA_FLAGS="--xla_gpu_autotune_level=2"
+  export XLA_FLAGS="--xla_gpu_autotune_level=2"
+  export XLA_PYTHON_CLIENT_MEM_FRACTION=0.8
   python -m src.scripts.train \
     --ansatz backflow \
     --config configs/one_dim_hubbard_fno_16.yaml \
