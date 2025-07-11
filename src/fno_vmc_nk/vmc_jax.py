8903f11430abb65d4fed66fcc8c71b7317761363
zhengshizhao <shizhao.zheng@mail.utoronto.ca>
2025-07-11 01:21:57 -0700

    Baseline Experiments on 1D

--- src/fno_vmc_nk/vmc_jax.py
+++ src/fno_vmc_nk/vmc_jax.py
@@ -43,7 +43,7 @@ class VMCTrainer:
         sampler = nk.sampler.MetropolisLocal(
             hilbert,
             n_chains_per_rank=32,
-            sweep_size=2,
+            sweep_size=1,
         )
 
         self._key = jax.random.PRNGKey(vmc_params.get("seed", 42))
