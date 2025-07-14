522cd51f0717643b72ddcccf1d62b55b836ed34e
zhengshizhao <shizhao.zheng@mail.utoronto.ca>
2025-07-14 03:28:18 -0700

    Baseline Experiments on 1D

--- src/scripts/run_train.sh
+++ src/scripts/run_train.sh
@@ -10,7 +10,8 @@ ssh -X shizhao@131.215.142.161 << 'EOF'
   # export XLA_FLAGS=--xla_gpu_enable_command_buffer=false
 
   # 用 cProfile 收集整个脚本的性能数据，输出到 prof.out
-  python -m cProfile -o prof.out src/scripts/train.py \
+  # python -m cProfile -o prof.out src/scripts/train.py \
+  python -m src.scripts.train \
     --ansatz backflow \
     --config configs/one_dim_hubbard_fno_16.yaml \
     --outdir resul/fno_run \
