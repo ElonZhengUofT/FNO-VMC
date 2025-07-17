bf816233aabf11305874554af2927eb523771c1b
zhengshizhao <shizhao.zheng@mail.utoronto.ca>
2025-07-16 18:36:46 -0700

    Creat Scale-Transfree Ansatz. Phase 1 Pretrain Dataset Generation

--- src/fno_vmc_nk/ansatz/fno_jax/__init__.py
+++ src/fno_vmc_nk/ansatz/fno_jax/__init__.py
@@ -1,6 +1,6 @@
 from .utils import get_grid, rfft2d, irfft2d
-from .spectral_conv import SpectralConv2d
-from .fno_jax import FNO2d
+from .spectral_conv import SpectralConv2d, SpectralConv1d
+from .fno_jax import FNO2d, FNO1d
 
 __all__ = [
     "get_grid", "rfft2d", "irfft2d",
